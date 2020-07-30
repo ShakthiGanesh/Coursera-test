dccb69c20d5f56a25637f13fe18757ee161bfa65
ShakthiGanesh <shakthi012000@gmail.com>
2020-07-30 13:07:06 +0530

    Module 4 Solution

---
+++ Module4-Soln/SpeakGoodBye.js
@@ -0,0 +1,14 @@
+
+(function (window) {
+
+var speakWord = "Good Bye";
+byeSpeaker = {
+	
+  speak: function (name) {
+  console.log(speakWord + " " + name)
+}
+};
+	window.byeSpeaker = byeSpeaker;
+})(window);
+
+
